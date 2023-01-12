1ce69f159d37ae47279756c2c626c72e0373c0b2
Jorge Vicente Chavez Vega <jorge.vicente2108@gmail.com>
2023-01-12 00:33:37 -0600

    vista para ver una tarjeta, con animaciones mamalonas

--- lib/src/ui/pages/finances/views/account_view.dart
+++ lib/src/ui/pages/finances/views/account_view.dart
@@ -6,11 +6,285 @@ class AcountView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Nueva cuenta'),
+      body: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            delegate: _HeaderDelegate(),
+            pinned: true,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Text(
+                'Movimientos bancarios',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              List.generate(
+                15,
+                (index) => Text('Item $index'),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 1000,
+              color: Theme.of(context).canvasColor,
+            ),
+          ),
+        ],
       ),
-      body: ListView(
-        children: [],
+    );
+  }
+}
+
+class _HeaderDelegate extends SliverPersistentHeaderDelegate {
+  final _maxHeaderExtend = 320.0;
+  final _minHeaderExtend = 180.0;
+
+  final _heightActionBottom = 60.0;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final size = MediaQuery.of(context).size;
+
+    final percent = shrinkOffset / _maxHeaderExtend;
+    print(percent);
+
+    return Stack(
+      children: [
+        Container(
+          color: Colors.blueAccent,
+          child: SafeArea(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        color:
+                            Theme.of(context).backgroundColor.withOpacity(0.5),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                      ),
+                      child: const BackButton(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            height: _heightActionBottom,
+            width: size.width,
+            decoration: BoxDecoration(
+              color: Theme.of(context).canvasColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40 * (1 - percent)),
+                topRight: Radius.circular(40 * (1 - percent)),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          left: 0,
+          top: 40,
+          child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: 1),
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.easeIn,
+            builder: (context, double value, child) {
+              return Transform.translate(
+                offset: Offset(0, -50 * (1 - value)),
+                child: child,
+              );
+            },
+            child: Column(
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  child: Icon(Icons.ac_unit),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          left: 0,
+          top: 90,
+          child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: 1),
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.easeIn,
+            builder: (context, double value, child) {
+              return Opacity(
+                opacity: value,
+                child: child,
+              );
+            },
+            child: Column(
+              children: const [
+                Text(
+                  'Tarjeta de debito',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          left: 0,
+          top: 110,
+          child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0, end: 1),
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.easeIn,
+            builder: (context, double value, child) {
+              return Opacity(
+                opacity: value,
+                child: child,
+              );
+            },
+            child: Column(
+              children: const [
+                Text(
+                  '\$450',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        _ActionButtons(
+          heightActionBottom: _heightActionBottom,
+          percent: percent,
+        ),
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => _maxHeaderExtend;
+
+  @override
+  double get minExtent => _minHeaderExtend;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
+}
+
+class _ActionButtons extends StatelessWidget {
+  final double _heightActionBottom;
+  final double percent;
+
+  const _ActionButtons({
+    Key? key,
+    required double heightActionBottom,
+    required this.percent,
+  })  : _heightActionBottom = heightActionBottom,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      left: 0,
+      bottom: _heightActionBottom * (1 - percent) + 10,
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: 0, end: 1),
+        duration: const Duration(milliseconds: 600),
+        curve: Curves.easeIn,
+        builder: (context, double value, child) {
+          return Transform.translate(
+            offset: Offset(0, 70 * (1 - value)),
+            child: child,
+          );
+        },
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Opacity(
+                  opacity: 1 - percent,
+                  child: const Text('Gasto'),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+                  child: const Icon(Icons.trending_down_rounded),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Opacity(
+                  opacity: 1 - percent,
+                  child: const Text('Ingreso'),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+                  child: const Icon(Icons.trending_up_rounded),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Opacity(
+                  opacity: 1 - percent,
+                  child: const Text('Transf.'),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+                  child: const Icon(Icons.transform_rounded),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
